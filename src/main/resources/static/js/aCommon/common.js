e6565a891c6ecd29002e2657bb744829a0c5e9cf
janggihun <jgh7111@gmail.com>
2024-01-26 00:30:17 +0900

    111

--- src/main/resources/static/js/aCommon/common.js
+++ src/main/resources/static/js/aCommon/common.js
@@ -1,5 +1,5 @@
-//const sse = new EventSource("http://192.168.0.27:8080/main"); // ipconfig를 통해 본인 아이피를 넣어줘야함
-const sse = new EventSource("http://localhost:8080/main"); //혼자할때
+const sse = new EventSource("http://192.168.0.27:8080/main"); // ipconfig를 통해 본인 아이피를 넣어줘야함
+//const sse = new EventSource("http://localhost:8080/main"); //혼자할때
 
 
 sse.addEventListener('connect', (e) => {
