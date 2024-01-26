5a2dff1f230cd8259a07444db16b63ebc9596d2d
user <user@DESKTOP-2GFHJSA>
2024-01-26 17:03:15 +0900

    20240126 정혜린 css 수정중

--- src/main/java/com/project/projectFinal/dao/JhlChampDao.java
+++ src/main/java/com/project/projectFinal/dao/JhlChampDao.java
@@ -6,36 +6,14 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.project.projectFinal.dto.JhlChampDto;
+
 @Mapper
 public interface JhlChampDao {
 
-	public List<HashMap<String, String>> ChampListImg1();
+	public List<HashMap<String, String>> champListImg1();
 
-	public List<HashMap<String, String>> ChampListImg2();
-
-	public List<HashMap<String, String>> ChampListImg3();
-
-	public List<HashMap<String, String>> ChampListImg4();
-
-	public List<HashMap<String, String>> ChampListImg5();
-
-	public List<HashMap<String, String>> ChampListImg6();
-
-	public List<HashMap<String, String>> ChampListImg7();
-
-	public List<HashMap<String, String>> ChampListImg8();
-
-	public List<HashMap<String, String>> ChampListImg9();
-
-	public List<HashMap<String, String>> ChampListImg10();
-
-	public List<HashMap<String, String>> ChampListImg11();
-
-	public List<HashMap<String, String>> ChampListImg12();
-
-	public List<HashMap<String, String>> ChampListImg13();
-
-	public List<HashMap<String, String>> ChampListImg14();
+	public List<HashMap<String, String>> champSearch(JhlChampDto champDto);
 
 	
 
