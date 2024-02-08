ed157e8682e863ace9acc273d4dfebfc83671b0a
thdxo <thdxo@DESKTOP-P8IBNUO>
2024-02-08 15:41:27 +0900

    송태민마감

--- src/main/resources/static/js/stm/RiotGameTable.js
+++ src/main/resources/static/js/stm/RiotGameTable.js
@@ -4,15 +4,13 @@
 
 
 function showGameTamble(res, data) {
-	console.log(data)
 	console.log(res)
 	let str = ''
 	//	for (let i = 0; i < res.length; i++) {
 	var timeStamp = Date.now();
 	gameEndTimestampString = res[0]["info"][0]['gameEndTimestamp']
-	console.log(gameEndTimestampString)
+
 	gameEndTimestamp = Number(gameEndTimestampString)
-	console.log(gameEndTimestamp)
 	loltime = (timeStamp - gameEndTimestamp) // ex) 몇일전입니다.
 
 	let day = Math.floor(loltime / 1000 / 60 / 60 / 24);
@@ -61,10 +59,10 @@ function showGameTamble(res, data) {
 		let kda = ''
 		let mykill = ''
 		let dragon = ''
-		
+
 		let upteamcolor = ''
 		let upteamwin = ''
-		
+
 		let downteamcolor = ''
 		let downteamwin = ''
 
@@ -79,10 +77,12 @@ function showGameTamble(res, data) {
 				queue = "자유랭크"
 			}
 			// 게임모드
-			if (res[i]["info"][j]['win'] == '1') {
-				win_lose = '승리'
-			} else {
-				win_lose = '패배'
+			if (data['gameName'] == res[i]["info"][j]['riotIdGameName'] || data['gameName'] == res[i]["info"][j]['summonerName']) {
+				if (res[i]["info"][j]['win'] == '1') {
+					win_lose = '승리'
+				} else {
+					win_lose = '패배'
+				}
 			}
 			// 승패
 			if (data['gameName'] == res[i]["info"][j]['riotIdGameName'] || data['gameName'] == res[i]["info"][j]['summonerName']) {
@@ -192,40 +192,92 @@ function showGameTamble(res, data) {
 		for (let j = 0; j < res[i]['info'].length; j++) {
 			if (res[i]['info'][j]['teamId'] == 100) {
 				upteamcolor = '블루팀'
-				downteamcolor ='레드팀'
-			}else{
-				upteamcolor = '레드팀'
-				downteamcolor = '블루팀'
+				if (res[i]['info'][j]['win'] == '1') {
+					upteamwin = '승리'
+				} else {
+					upteamwin = '패배'
+				}
+
 			}
-			if (res[i]['info'][j]['win'] == '1') {
-				upteamwin = '승리'
-				downteamwin ='패배'
-			}else{
-				upteamwin = '패배'
-				downteamwin ='승리'
+
+			if (res[i]['info'][j]['teamId'] == 200) {
+				downteamcolor = '레드팀'
+				if (res[i]['info'][j]['win'] == '1') {
+					downteamwin = '승리'
+				} else {
+					downteamwin = '패배'
+				}
+
 			}
 		}
+
 
 		str += "<table class='resultwin'style='display: none' id = 'gametable" + i + "'> " //
 		str += "<thead>"
 		str += "<tr>"
-		str += "<th colspan='2' class='th'>" + upteamwin + "("+upteamcolor+")</th>"
-		str += "<th class='th'>K/D/A</th>"
-		str += "<th  class='th'>아군받은피해량</th>"
-		str += "<th  class='th'>아군가한피해량</th>"
-		str += "<th class='th'>CS</th>"
+		str += "<th colspan='2' class='th'>" + upteamwin + "(" + upteamcolor + ")</th>"
+		str += "<th class='th'>KDA(평점)</th>"
+		str += "<th  class='th'>블루팀받은피해량</th>"
+		str += "<th  class='th'>블루팀가한피해량</th>"
+		str += "<th class='th'>총합 CS</th>"
 		str += "<th colspan='2' class='th'>아군아이템</th>"
 		str += "</tr>"
 		str += "</thead>"
 		str += "<tbody>"
+
+		let damageToChampions = []
+		let damageTaken = []
+		for (let j = 0; j < res[i]['info'].length; j++) {
+			damageToChampions.push(res[i]["info"][j]['totalDamageDealtToChampions'])
+			damageTaken.push(res[i]["info"][j]['totalDamageTaken'])
+		}
+
+		highestdamageToChampions = Math.max(...damageToChampions)
+		highdamageTaken = Math.max(...damageTaken)
 		for (let j = 0; j < res[i]['info'].length; j++) { // 팀 아이디 100
+
+
+
+
 			if (res[i]['info'][j]['teamId'] == 100) {
+				teamchampimg = res[i]["info"][j]['championName']
+				teamchampKrName = res[i]["info"][j]['champion_name_kr']
+				teamkills = res[i]["info"][j]['kills'] + "/"
+				teamdeaths = res[i]["info"][j]['deaths'] + "/"
+				teamassists = res[i]["info"][j]['assists']
+				teamKDA = res[i]["info"][j]['kda']
+				totalMinionsKilled = res[i]["info"][j]['totalMinionsKilled']
+				wardsKilled = res[i]["info"][j]['wardsKilled']
+				totalAllyJungleMinionsKilled = res[i]["info"][j]['totalAllyJungleMinionsKilled']
+				Number(totalMinionsKilled);
+				Number(wardsKilled);
+				Number(totalAllyJungleMinionsKilled);
+				cs = totalMinionsKilled + wardsKilled + totalAllyJungleMinionsKilled
+				totalDamageDealtToChampions = res[i]["info"][j]['totalDamageDealtToChampions'] //챔피언에게가한피해량
+				totalDamageTaken = res[i]["info"][j]['totalDamageTaken'] //받은 총 피해량
+
+				bartotalDamageDealtToChampions = (((totalDamageDealtToChampions / highestdamageToChampions) * 99)).toFixed(0)
+
+				bartotalDamageTaken = (((totalDamageTaken / highdamageTaken) * 99)).toFixed(0)
+				//가장 높은 딜량을 기준으로 나눠야할듯
 				str += "<tr>"
-				str += "<td colspan='2' class='cimg'>1</td>"
-				str += "<td class='K/D/A'></td>"
-				str += "<td class='damageDealt'></td>"
-				str += "<td class='damageReceived'></td>"
-				str += "<td class='cs'></td>"
+				str += "<td colspan='2' class='cimg'><img width='23' height='23'  alt='못 불러옴' src='https://ddragon.leagueoflegends.com/cdn/14.1.1/img/champion/" + teamchampimg + ".png'>[" + teamchampKrName + "]</td>"
+				str += "<td class='K/D/A'>" + teamkills + teamdeaths + teamassists + "(" + teamKDA + ")" + "</td>"
+				str += "<td class='damageDealt'>"
+				str += `
+				<div class="RiotGamechart">
+    			<div class="RiotGamebar" style="width: ${bartotalDamageDealtToChampions}%;">${totalDamageDealtToChampions}</div>
+				</div>
+				`
+				str += "</td>"
+				str += "<td class='damageReceived'>"
+				str += `
+				<div class="RiotGamechart">
+    			<div class="RiotGamebar2" style="width: ${bartotalDamageTaken}%;">${totalDamageTaken}</div>
+				</div>
+				`
+				str += "</td>"
+				str += "<td class='cs'>" + cs + "</td>"
 				str += "<td class='item'>"
 				str += "<div class='Titemlist'>"
 				str += "<div class='Titem1'></div>"
@@ -242,22 +294,54 @@ function showGameTamble(res, data) {
 		str += "</tbody>"
 		str += "<thead>"
 		str += "<tr>"
-		str += "<th colspan='2' class='th'>" + downteamwin + "("+downteamcolor+")</th>"
-		str += "<th class='th'>K/D/A</th>"
-		str += "<th  class='th'>적군받은피해량</th>"
-		str += "<th  class='th'>적군가한피해량</th>"
-		str += "<th class='th'>CS</th>"
+		str += "<th colspan='2' class='th'>" + downteamwin + "(" + downteamcolor + ")</th>"
+		str += "<th class='th'>KDA(평점)</th>"
+		str += "<th  class='th'>레드팀받은피해량</th>"
+		str += "<th  class='th'>레드팀가한피해량</th>"
+		str += "<th class='th'>총합 CS</th>"
 		str += "<th colspan='2' class='th'>적군아이템</th>"
 		str += "</tr>"
 		str += "</thead>"
 		for (let j = 0; j < res[i]['info'].length; j++) { // 팀 아이디 100
 			if (res[i]['info'][j]['teamId'] == 200) {
+				teamchampimg = res[i]["info"][j]['championName']
+				teamchampKrName = res[i]["info"][j]['champion_name_kr']
+				teamkills = res[i]["info"][j]['kills'] + "/"
+				teamdeaths = res[i]["info"][j]['deaths'] + "/"
+				teamassists = res[i]["info"][j]['assists']
+				teamKDA = res[i]["info"][j]['kda']
+				totalMinionsKilled = res[i]["info"][j]['totalMinionsKilled']
+				wardsKilled = res[i]["info"][j]['wardsKilled']
+				totalAllyJungleMinionsKilled = res[i]["info"][j]['totalAllyJungleMinionsKilled']
+				Number(totalMinionsKilled);
+				Number(wardsKilled);
+				Number(totalAllyJungleMinionsKilled);
+				cs = totalMinionsKilled + wardsKilled + totalAllyJungleMinionsKilled
+
+				totalDamageDealtToChampions = res[i]["info"][j]['totalDamageDealtToChampions'] //챔피언에게가한피해량
+				totalDamageTaken = res[i]["info"][j]['totalDamageTaken'] //받은 총 피해량
+
+				bartotalDamageDealtToChampions = (((totalDamageDealtToChampions / highestdamageToChampions) * 99)).toFixed(0)
+
+				bartotalDamageTaken = (((totalDamageTaken / highdamageTaken) * 99)).toFixed(0)
 				str += "<tr>"
-				str += "<td colspan='2' class='cimg'>1</td>"
-				str += "<td class='K/D/A'></td>"
-				str += "<td class='damageDealt'></td>"
-				str += "<td class='damageReceived'></td>"
-				str += "<td class='cs'></td>"
+				str += "<td colspan='2' class='cimg'><img width='23' height='23'  alt='못 불러옴' src='https://ddragon.leagueoflegends.com/cdn/14.1.1/img/champion/" + teamchampimg + ".png'>[" + teamchampKrName + "]</td>"
+				str += "<td class='K/D/A'>" + teamkills + teamdeaths + teamassists + "(" + teamKDA + ")" + "</td>"
+				str += "<td class='damageDealt'>"
+				str += `
+				<div class="RiotGamechart">
+    			<div class="RiotGamebar" style="width: ${bartotalDamageDealtToChampions}%;">${totalDamageDealtToChampions}</div>
+				</div>
+				`
+				str += "</td>"
+				str += "<td class='damageReceived'>"
+				str += `
+				<div class="RiotGamechart">
+    			<div class="RiotGamebar2" style="width: ${bartotalDamageTaken}%;">${totalDamageTaken}</div>
+				</div>
+				`
+				str += "</td>"
+				str += "<td class='cs'>" + cs + "</td>"
 				str += "<td class='item'>"
 				str += "<div class='Titemlist'>"
 				str += "<div class='Titem1'></div>"
@@ -278,7 +362,26 @@ function showGameTamble(res, data) {
 
 	}
 	str += "</div>"
+	str += "<button class='more' id = 'loadMore'>더 보기</button>"
+	str += "<div id ='asd'></div>"
 	str += "</div>"
 	$('#puuid').append(str)
 	$('#puuid').show();
+	let como = 1;
+	$("#loadMore").on("click", function() {
+//		const asd = document.getElementById('asd');
+//		
+		loadMore.style.top = '30%'
+//		const newH1 = document.createElement('h1');
+//		newH1.textContent = 'asd';
+//		for (let i = 0; i < 5; i++) {
+//		asd.appendChild(newH1);
+//
+//
+//		}
+		$("#asd").append("<p class='original' style='top:32%; position : absolute;;'>등장"+como+"</p>");
+		como +=1
+		console.log(res.length)
+
+	})
 }
