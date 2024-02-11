cba08db4721e9b7ba6c6765854ee9ecb018b28d1
janggihun <jgh7111@gmail.com>
2024-02-11 19:22:27 +0900

    123

--- src/main/resources/static/js/jgh/riotTv/main.js
+++ src/main/resources/static/js/jgh/riotTv/main.js
@@ -1,11 +1,23 @@
 /**
  * 
  */
+let gameName1 = $('#gameName1').val()
+let tagLine = $('#tagLine').val()
+let championName = ''
+let teamId = ''
+let matchId = ''
+let dbList = []
+let damageTakenOnTeamPercentage = ''
+let kda = ''
+let killParticipation = ''
+let teamDamagePercentage = ''
+let goldPerMinute = ''
+
+
 
 $("#find").on("click", function() {
 	$('#detail2').empty()
-	let gameName1 = $('#gameName1').val()
-	let tagLine = $('#tagLine').val()
+
 	let str = ''
 	data = {
 		'gameName': gameName1,
@@ -35,18 +47,58 @@ $("#find").on("click", function() {
 						for (let j in res.info.participants) {
 
 							if (res.info.participants[j].riotIdGameName == gameName1) {
+
 								str += z + "번째 경기" + '<input type = "button" onclick = "findOne(this.id)" id = "' + result1[z] + '" value = ">>라문철tv분석<<">'
-									+" 내가 한 챔프 :"+ res.info.participants[j].championName + '</br>'
-								console.log(res.info.participants[j].championName)
+								//									+ " 내가 한 챔프 :" + res.info.participants[j].championName 
+
+								str += "내 챔프 : <img width='50' height='50'  alt='못 불러옴' src='https://ddragon.leagueoflegends.com/cdn/14.1.1/img/champion/" + res.info.participants[j].championName + ".png'>"
+								str += teamId + '</br>'
 
 							}
+
+							matchId = res.metadata.matchId
+							championName = res.info.participants[j].championName
+							teamId = res.info.participants[j].teamId
+							damageTakenOnTeamPercentage = res.info.participants[j].challenges.damageTakenOnTeamPercentage
+							kda=res.info.participants[j].challenges.kda
+							killParticipation = res.info.participants[j].challenges.killParticipation
+							teamDamagePercentage= res.info.participants[j].challenges.teamDamagePercentage
+							goldPerMinute = res.info.participants[j].challenges.goldPerMinute
+							
+							
+							
+							db = {}
+							db.matchId = matchId
+							db.championName = championName
+							db.teamId = teamId
+							db.damageTakenOnTeamPercentage=damageTakenOnTeamPercentage
+							db.kda = kda
+							db.killParticipation=killParticipation
+							db.teamDamagePercentage=teamDamagePercentage
+							db.goldPerMinute =goldPerMinute
+							
+							
+							
+							dbList.push(db)
+							console.log(res)
 						}
+
 						$('#detail2').html(str)
+
+						//프론트에 내용 보내기(위에)
+						//db에 내용 보내기 (아래)
+
+
+						dbSaveInfoRiotTv(res)
+
 					}
+
 				})
+
 			}
 		}
 	})
+	console.log(dbList)
 })
 
 function findOne(matchIdjustOne) {
@@ -56,3 +108,41 @@ function findOne(matchIdjustOne) {
 	console.log(data)
 
 }
+
+function dbSaveInfoRiotTv(res) {
+
+	let matchId = res.metadata.matchId
+	var date = new Date(res.info.gameStartTimestamp);
+
+	console.log(
+		date.getFullYear() + '/' + (date.getMonth() + 1) + '/' +
+		date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' +
+		date.getSeconds());
+
+	for (let i in res.info.participants) {
+
+
+
+
+	}
+
+
+}
+
+
+
+//function changeNameEngToKor(engName) {
+//
+//	data = { 'engName': engName }
+//	$.ajax({
+//
+//		type: 'post',
+//		url: '/riotTv/changNameEngToKor',
+//		//		async: false,
+//		data: data2,
+//		success: function(res) {
+//
+//
+//		}
+//	})
+//}
